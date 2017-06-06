68dd7d52ada58dfb8fc85a796d1b0b1184b83428
Matt Groom <matthew.groom@ntlworld.com>
2017-06-06 14:42:15 +0100

    session 4 submission

--- app/controllers/WelcomeController.scala
+++ app/controllers/WelcomeController.scala
@@ -1,13 +1,18 @@
 
 package controllers
 
+import javax.inject.Inject
+
 import play.api.mvc.{Action, Controller}
+import services.GreetingService
 
-class WelcomeController extends Controller {
 
+class WelcomeController @Inject() (greeter: GreetingService) extends Controller{
   def welcome() = Action {
-    Ok(views.html.welcome())
-
+    val greeting = greeter.greeting
+    Ok(views.html.welcome(greeting))
   }
-
 }
+
+
+
